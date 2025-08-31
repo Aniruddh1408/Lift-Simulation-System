cb37d229a791c85d2810b5eca68e386a67062bda
Aniruddh1408 <aniruddhachars@gmail.com>
2025-08-31 13:19:09 +0530

    Created the main program that operates the Lift

--- src/com/liftsim/LiftSystem.java
+++ src/com/liftsim/LiftSystem.java
@@ -1,4 +1,19 @@
 package com.liftsim;
 
+import com.liftsim.controller.LiftController;
+import com.liftsim.gui.LiftGUI;
+import com.liftsim.model.Lift;
+
+import javax.swing.*;
+
 public class LiftSystem {
+    public static void main(String[] args) {
+        // Run GUI in Swing's Event Dispatch Thread (best practice for Swing apps)
+        SwingUtilities.invokeLater(() -> {
+            Lift lift = new Lift();                      // Create lift model
+            LiftController controller = new LiftController(lift); // Controller
+            LiftGUI gui = new LiftGUI(controller);       // GUI
+            gui.showGUI();                               // Display
+        });
+    }
 }
